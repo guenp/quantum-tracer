aa0112ae742e7905a314fad09983e15bf1d4bcc2
Guen Prawiroatmodjo <guenp@microsoft.com>
2021-10-14 16:19:10 -0700

    remove rewrite step

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
-
 module.exports = {
   assetPrefix: isProd ? '/quantum-tracer/' : '',
-  basePath: isProd ? '/quantum-tracer': '',
-  async rewrites() {
-    return [
-      {
-        source: '/:any*',
-        destination: '/',
-      },
-    ];
-  },
 };
